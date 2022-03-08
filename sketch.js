a62e7c95ce2bdd90a66f34da356276cbd30e46a2
Justin Singh <justinsingh2015@gmail.com>
2022-03-08 13:52:36 -0800

    Implement blue rectangle strip

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1280, 1080);
 }
 
 function draw() {
-  background(220);
+  // Set Constants
+  // Colors
+  const teal = color(180, 238, 243);
+  const white = color(255);
+  const black = color(20);
+  
+  // Positions
+  const rectXStart = 175;
+  const rectYStart = 750;
+  const rectXEnd = 975;
+  
+  // Shape attributes
+  const rectWidth = 3.25;
+  const rectHeight = 250;
+        
+  // Generate Art
+  // Set Background
+  background(black);
+
+  // Place Strip of rectangles
+  let xStart = rectXStart;
+  while (xStart <= rectXEnd) {
+    // Render rectangle
+    fill(teal);
+    noStroke();
+    rect(xStart, rectYStart, rectWidth, rectHeight, 20);
+    
+    // Increment xStart by desired white space amount
+    xStart += rectWidth * 2;
+  }
 }
