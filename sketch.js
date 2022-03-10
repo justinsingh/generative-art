87a7f5e4846e7b08e5ac4895844c3d2e8d6103c6
Justin Singh <justinsingh2015@gmail.com>
2022-03-09 18:14:02 -0800

    Add second line

--- sketch.js
+++ sketch.js
@@ -38,6 +38,7 @@ THINGS TO TRY:
 - warp/transform rectangle strips
 - random vs noise (perlin)
 - rectangle over lines, or lines over rectangles?
+- modify number of lines
 */
 function setup() {
   createCanvas(1210, 1580);
@@ -51,9 +52,11 @@ function draw() {
   const black = color(20);
   const cream = color(255, 252, 240);
   const orange = color("orange");
+  const red = color("red");
+  const blue = color("blue");
 
   // Positions
-  const rectXStart = 450;
+  const rectXStart = 500;
   const rectYStart = 1180;
   const rectXEnd = 1145;
 
@@ -62,10 +65,13 @@ function draw() {
   const lineXEnd = 600;
   const lineYEnd = 790;
 
+  const line2XStart = -40;
+  const line2YStart = 70;
+
   // Shape attributes
   const rectWidth = 5.25;
   const rectHeight = 250;
-  const rectColor = teal;
+  const rectColor = red;
 
   const lineStrokeWidth = 1.2;
   const lineColor = black;
@@ -74,14 +80,16 @@ function draw() {
   // Set Background
   background(cream);
 
+  // Set random seed
+  randomSeed(10);
+
   // Place Bottom Right Lines
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 50; i++) {
     // Set stroke properties
     stroke(lineColor);
     strokeWeight(lineStrokeWidth);
     noFill();
 
-    // Random num
     let rand = random(-50, 50);
 
     // Use bezier() function to create curve
@@ -89,13 +97,33 @@ function draw() {
       lineXStart + random(25),
       lineYStart + random(25),
       780 + random(-250, -200),
-      730 + random(250),
-      735 + random(100, 300),
-      675 + random(-500),
+      730 + random(350),
+      735 + random(300, 500),
+      675 + random(-600),
       lineXEnd + random(25),
       lineYEnd + random(25)
     );
   }
+
+  // Place Top Left Lines
+  for (let i = 0; i < 50; i++) {
+    // Set stroke properties
+    stroke(lineColor);
+    strokeWeight(lineStrokeWidth);
+    noFill();
+
+    // Use bezier() function to create curve
+    bezier(
+      line2XStart + random(25),
+      line2YStart + random(25),
+      290 + random(200),
+      1500 + random(-250),
+      500 + random(-300),
+      800 + random(200),
+      lineXEnd + random(10, 20),
+      lineYEnd + random(25)
+    );
+  } 
 
   // Place Strip of rectangles
   let xStart = rectXStart;
@@ -109,7 +137,6 @@ function draw() {
     xStart += rectWidth * 2;
   }
 
-  randomSeed(10);
 
 
 
