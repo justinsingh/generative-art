6200f4882c93af022e770a7fd8f803358293b24f
Justin Singh <justinsingh2015@gmail.com>
2022-04-17 22:58:20 -0700

    Adjust positioning

--- perlin-noise-loop.js
+++ perlin-noise-loop.js
@@ -14,27 +14,30 @@ function draw() {
   const blue = color(0, 0, 255, 150);
 
   background(cream);
-  translate(width / 2, height / 2);
+  //translate(width / 2, height / 2);
   strokeWeight(15);
-  stroke(red);
+  stroke(black);
   noFill();
 
-  for (let i = 0; i < 1; i++) {
-    //drawNoisyCircle();
-    drawNoisyLine(PI / 4, 15, orange);
+  for (let i = 0; i < 3; i++) {
+    drawNoisyCircle();
+    //drawNoisyLine(0, PI/ 16, 15, orange);
+    //drawNoisyLine(PI / 12, PI / 8, 15, teal);
+    //drawNoisyLine(PI / 4, PI / 2, 15, blue);
   }
-}
 
 function drawNoisyCircle() {
   noiseSeed(random(0, 100));
+
+  strokeWeight(15);
   beginShape();
   for (let a = 0; a < TWO_PI; a += 0.01) {
     let xOff = map(cos(a), -1, 1, 0, noiseMax);
     let yOff = map(sin(a), -1, 1, 0, noiseMax);
 
     let r = map(noise(xOff, yOff), 0, 1, 100, 400);
-    let x = r * cos(a);
-    let y = r * sin(a);
+    let x = r * cos(a) + width / 2;
+    let y = r * sin(a) + height / 2;
 
     vertex(x, y);
   }
@@ -43,13 +46,13 @@ function drawNoisyCircle() {
   noLoop();
 }
 
-function drawNoisyLine(shapeLength, strokeSize, strokeColor) {
+function drawNoisyLine(shapeStart, shapeEnd, strokeSize, strokeColor) {
   strokeWeight(strokeSize);
   stroke(strokeColor);
 
   noiseSeed(random(0, 100));
   beginShape();
-  for (let i = 0; i < shapeLength; i += 0.01) {
+  for (let i = shapeStart; i < shapeEnd; i += 0.01) {
     let xOff = map(cos(i), -1, 1, 0, noiseMax);
     let yOff = map(sin(i), -1, 1, 0, noiseMax);
 
@@ -62,4 +65,4 @@ function drawNoisyLine(shapeLength, strokeSize, strokeColor) {
 
   endShape();
   noLoop();
-}
+}}
